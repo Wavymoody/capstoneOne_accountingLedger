b55339662df253408d3ecb63a9c2c7cd32ee66f4
Mahmoud <melbayoumi051@gmail.com>
2025-04-29 14:52:52 -0400

    first commit

---
+++ src/main/java/com/ps/Main.java
@@ -0,0 +1,46 @@
+package com.ps;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    static Scanner scanner = new Scanner(System.in);
+
+
+//    static ArrayList<Product> inventory = new ArrayList<>();
+//    static ArrayList<Product> cart = new ArrayList<>();
+
+    public static void main(String[] args) {
+//        loadProducts();
+
+        int homeScreenMenu;
+
+        do {
+            System.out.println("Welcome the Moody Bank");
+            System.out.println("1. Add Deposit");
+            System.out.println("2. Make Payment");
+            System.out.println("3. Ledger");
+            System.out.println("4. Exit \n Enter your choice");
+            homeScreenMenu = scanner.nextInt();
+            switch (homeScreenMenu) {
+                case 1:
+                    addDeposit();
+                    break;
+                case 2:
+                    makePayment();
+                    break;
+                case 3:
+                    showLedgerScreen();
+                    break;
+                case 4:
+                    System.out.println("Exit Program");
+
+                default:
+                    System.out.println("Incorrect Input! Try again.");
+            }
+        } while (homeScreenMenu != 4);
+    }
+}
