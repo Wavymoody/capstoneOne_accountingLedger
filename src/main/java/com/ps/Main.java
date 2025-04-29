669641ac86c1c07f30ad5c6a663fc0a639a577ac
Mahmoud <melbayoumi051@gmail.com>
2025-04-29 14:56:18 -0400

    created home screen

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -44,3 +44,36 @@ public class Main {
         } while (homeScreenMenu != 4);
     }
 }
+
+private static void showLedgerScreen() {
+    System.out.println(" Please enter your choice");
+    System.out.println("1. All");
+    System.out.println("2. Deposit");
+    System.out.println("3. Payment");
+    System.out.println("4. Report");
+    System.out.println("5. Home");
+
+    int ledgerScreenint = scanner.nextInt();
+    switch (ledgerScreenint) {
+        case 1:
+            displayEntries();
+            break;
+        case 2:
+            displayDeposit();
+            break;
+        case 3:
+            displayPayment();
+            break;
+
+        case 4:
+            displayReport();
+            break;
+
+        case 5:
+            displayHome();
+            break;
+        default:
+            System.out.println("Incorrect Input! Try again.");
+
+    }
+}
